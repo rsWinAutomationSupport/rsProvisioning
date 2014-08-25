14934538bae6f4d8a08bb2c057b49bbe0f805741
PULLSERVER <PULLSERVER@localhost.local>
2014-08-25 13:09:26 -0500

    PULLSERVER pushing PullServerInfo.ps1

--- rsBasePrep.ps1
+++ rsBasePrep.ps1
@@ -128,7 +128,6 @@ Function Create-SshKey {
       $sshKey = Get-Content -Path "C:\Program Files (x86)\Git\.ssh\id_rsa.pub"
       $json = @{"title" = "$($d.DDI + "_" + $env:COMPUTERNAME)"; "key" = "$sshKey"} | ConvertTo-Json
       Invoke-RestMethod -Uri "https://api.github.com/user/keys" -Headers @{"Authorization" = "token $($d.gAPI)"} -Body $json -ContentType application/json -Method Post
-      chdir (Join-Path $d.wD -ChildPath $d.mR)
       Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "config --system user.email $serverName@localhost.local"
       Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "config --system user.name $serverName"
    }
