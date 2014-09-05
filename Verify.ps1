6cd7a7d33d044eaca6510658b264d08ee99ed682
Mark Key <mark.key@rackspace.com>
2014-09-05 15:56:39 -0500

    bugfix

--- Verify.ps1
+++ Verify.ps1
@@ -113,7 +113,7 @@ Function Install-Certs {
    Get-ChildItem Cert:\LocalMachine\Root\ | where {$_.Subject -eq $cN} | Remove-Item
    Copy-Item -Path $($d.wD, $d.mR, "Certs\id_rsa.txt" -join '\') -Destination 'C:\Program Files (x86)\Git\.ssh\id_rsa'
    Copy-Item -Path $($d.wD, $d.mR, "Certs\id_rsa.pub" -join '\') -Destination 'C:\Program Files (x86)\Git\.ssh\id_rsa.pub'
-   powershell.exe certutil -addstore -f root $($d.wD, "PullServer.cert.pfx" -join '\')
+   powershell.exe certutil -addstore -f root $($d.wD, $d.mR, "Certs\PullServer.cert.pfx" -join '\')
 }
 $role = Get-Role
 if($role -eq "Pull") {
