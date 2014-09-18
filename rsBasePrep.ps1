bf316475bd7d7c1d65eb0b95d1a0350c1cb120e1
Mark Key <mark.key@rackspace.com>
2014-09-18 10:05:51 -0500

    removed count

--- rsBasePrep.ps1
+++ rsBasePrep.ps1
@@ -103,7 +103,7 @@ Function Check-RC {
       try {
          Write-EventLog -LogName DevOps -Source BasePrep -EntryType Information -EventId 1000 -Message "Retrieving region from xenstore"
          $dc = $session.GetValue("vm-data/provider_data/region").value
-         if($dc.count -gt 0) {
+         if($dc) {
             $isDone = $true
          }
          else {
