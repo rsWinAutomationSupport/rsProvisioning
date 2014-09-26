c266db87646a74dd30aeb82d9b5acacefb730552
Mark Key <mark.key@rackspace.com>
2014-09-26 08:05:31 -0500

    added missing }

--- Verify.ps1
+++ Verify.ps1
@@ -1,7 +1,7 @@
 ﻿. "C:\cloud-automation\secrets.ps1"
 . "$($d.wD, $d.mR, "PullServerInfo.ps1" -join '\')"
 try {
-   $basePrepState = (Get-ScheduledTask -TaskName "BasePrep").State
+   $basePrepState = (Get-ScheduledTask -TaskName "BasePrep" -ErrorAction SilentlyContinue).State
 }
 catch {
 }
@@ -51,15 +51,15 @@ Function Download-File {
 }
 ### will pull before running rsEnvironments.ps1
 Function Check-Hash {
-if((Test-Path $($d.wD, "rsEnvironments.hash" -join '\')) -eq $false) {
-   Set-Content -Path $($d.wD, "rsEnvironments.hash" -join '\') -Value (Get-FileHash -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')).hash
-   chdir $($d.wD, $d.mR -join '\')
-   Start-Service Browser
-   Start -Wait git pull
-   Stop-Service Browser
-   taskkill /F /IM WmiPrvSE.exe
-   Invoke-Command -ScriptBlock { PowerShell.exe $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')} -ArgumentList "-ExecutionPolicy Bypass -Force"
-   ### Watch Pullserver DSC install proccess and wait for completion
+   if((Test-Path $($d.wD, "rsEnvironments.hash" -join '\')) -eq $false) {
+      Set-Content -Path $($d.wD, "rsEnvironments.hash" -join '\') -Value (Get-FileHash -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')).hash
+      chdir $($d.wD, $d.mR -join '\')
+      Start-Service Browser
+      Start -Wait git pull
+      Stop-Service Browser
+      taskkill /F /IM WmiPrvSE.exe
+      Invoke-Command -ScriptBlock { PowerShell.exe $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')} -ArgumentList "-ExecutionPolicy Bypass -Force"
+      ### Watch Pullserver DSC install proccess and wait for completion
       <#if(!(Test-Path -Path "C:\Windows\System32\Configuration\Current.mof") -and (Test-Path -Path "C:\Windows\System32\Configuration\Pending.mof")) {
          do {
             if((Get-ScheduledTask -TaskName "Consistency").State -eq "Ready") {
@@ -72,14 +72,14 @@ if((Test-Path $($d.wD, "rsEnvironments.hash" -join '\')) -eq $false) {
          }
          while(!(Test-Path -Path "C:\Windows\System32\Configuration\Current.mof"))
       }#>
-}
+   }
    $checkHash = Get-FileHash $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')
    $currentHash = Get-Content $($d.wD, "rsEnvironments.hash" -join '\')
-if($checkHash.Hash -ne $currentHash) {
-   Set-Content -Path $($d.wD, "rsEnvironments.hash" -join '\') -Value (Get-FileHash -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')).hash
-   taskkill /F /IM WmiPrvSE.exe
-   Invoke-Command -ScriptBlock { PowerShell.exe $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')} -ArgumentList "-ExecutionPolicy Bypass -Force"
-   ### Watch Pullserver DSC install proccess and wait for completion
+   if($checkHash.Hash -ne $currentHash) {
+      Set-Content -Path $($d.wD, "rsEnvironments.hash" -join '\') -Value (Get-FileHash -Path $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')).hash
+      taskkill /F /IM WmiPrvSE.exe
+      Invoke-Command -ScriptBlock { PowerShell.exe $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')} -ArgumentList "-ExecutionPolicy Bypass -Force"
+      ### Watch Pullserver DSC install proccess and wait for completion
       <#if(!(Test-Path -Path "C:\Windows\System32\Configuration\Current.mof") -and (Test-Path -Path "C:\Windows\System32\Configuration\Pending.mof")) {
          do {
             Write-EventLog -LogName DevOps -Source Verify -EntryType Information -EventId 1002 -Message "Current.mof has not yet been created and Pending.mof does exist."
@@ -93,11 +93,11 @@ if($checkHash.Hash -ne $currentHash) {
          }
          while(!(Test-Path -Path "C:\Windows\System32\Configuration\Current.mof"))
       }#>
-   
-   else {
-      taskkill /F /IM WmiPrvSE.exe
-      Invoke-Command -ScriptBlock { PowerShell.exe $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')} -ArgumentList "-ExecutionPolicy Bypass -Force"
-      ### Watch Pullserver DSC install proccess and wait for completion
+      
+      else {
+         taskkill /F /IM WmiPrvSE.exe
+         Invoke-Command -ScriptBlock { PowerShell.exe $($d.wD, $d.mR, "rsEnvironments.ps1" -join '\')} -ArgumentList "-ExecutionPolicy Bypass -Force"
+         ### Watch Pullserver DSC install proccess and wait for completion
          <#do {
             Write-EventLog -LogName DevOps -Source Verify -EntryType Information -EventId 1002 -Message "Current.mof has not yet been created and Pending.mof does not exist."
             if((Get-ScheduledTask -TaskName "Consistency").State -eq "Ready") {
@@ -110,34 +110,35 @@ if($checkHash.Hash -ne $currentHash) {
          }
          while(!(Test-Path -Path "C:\Windows\System32\Configuration\Current.mof"))
       }#>
-      
-      $pullServerName = $env:COMPUTERNAME
-      $pullServerPrivateIP = (Get-NetAdapter | ? status -eq 'up' | Get-NetIPAddress -ea 0 | ? IPAddress -match '^10\.').IPAddress
-      $pullServerPublicIp = Get-AccessIPv4
-      $path = $($d.wD, $d.mR, "PullServerInfo.ps1" -join '\')
-      if(Test-Path -Path $path) {
-         Remove-Item -Path $path -Force
-      }
-      $region = Get-Region
-      chdir $($d.wD, $d.mR -join '\')
-      New-Item -path $path -ItemType file
-      Add-Content -Path $path -Value "`$pullServerInfo = @{"
-      Add-Content -Path $path -Value "`"pullServerName`" = `"$pullServerName`""
-      Add-Content -Path $path -Value "`"pullServerPrivateIp`" = `"$pullServerPrivateIp`""
-      Add-Content -Path $path -Value "`"pullServerPublicIp`" = `"$pullServerPublicIp`""
-      Add-Content -Path $path -Value "`"region`" = `"$region`""
-      Add-Content -Path $path -Value "`"isRackConnect`" = `$$($isRackConnect.toString().toLower())"
-      Add-Content -Path $path -Value "`"isManaged`" = `$$($isManaged.toString().toLower())"
-      Add-Content -Path $path -Value "`"defaultRegion`" = `"$defaultRegion`""
-      Add-Content -Path $path -Value "}"
-      Set-Service Browser -startuptype "manual"
-      Start-Service Browser
-      Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "add $($d.wD + "\" + $d.mR + "\" + "PullServerInfo.ps1")"
-      Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "commit -am `"$pullServerName pushing PullServerInfo.ps1`""
-      Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "pull origin $($d.br)"
-      Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "push origin $($d.br)"
-      Stop-Service Browser
-   }  
+         
+         $pullServerName = $env:COMPUTERNAME
+         $pullServerPrivateIP = (Get-NetAdapter | ? status -eq 'up' | Get-NetIPAddress -ea 0 | ? IPAddress -match '^10\.').IPAddress
+         $pullServerPublicIp = Get-AccessIPv4
+         $path = $($d.wD, $d.mR, "PullServerInfo.ps1" -join '\')
+         if(Test-Path -Path $path) {
+            Remove-Item -Path $path -Force
+         }
+         $region = Get-Region
+         chdir $($d.wD, $d.mR -join '\')
+         New-Item -path $path -ItemType file
+         Add-Content -Path $path -Value "`$pullServerInfo = @{"
+         Add-Content -Path $path -Value "`"pullServerName`" = `"$pullServerName`""
+         Add-Content -Path $path -Value "`"pullServerPrivateIp`" = `"$pullServerPrivateIp`""
+         Add-Content -Path $path -Value "`"pullServerPublicIp`" = `"$pullServerPublicIp`""
+         Add-Content -Path $path -Value "`"region`" = `"$region`""
+         Add-Content -Path $path -Value "`"isRackConnect`" = `$$($isRackConnect.toString().toLower())"
+         Add-Content -Path $path -Value "`"isManaged`" = `$$($isManaged.toString().toLower())"
+         Add-Content -Path $path -Value "`"defaultRegion`" = `"$defaultRegion`""
+         Add-Content -Path $path -Value "}"
+         Set-Service Browser -startuptype "manual"
+         Start-Service Browser
+         Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "add $($d.wD + "\" + $d.mR + "\" + "PullServerInfo.ps1")"
+         Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "commit -am `"$pullServerName pushing PullServerInfo.ps1`""
+         Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "pull origin $($d.br)"
+         Start -Wait -NoNewWindow "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "push origin $($d.br)"
+         Stop-Service Browser
+      }  
+   }
 }
 ### Client tasks
 Function Check-Hosts {
