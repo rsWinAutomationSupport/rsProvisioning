48cdaf664a9b8c1b8e88d1e49e0ca5a33dae61d5
Mark Key <mark.key@rackspace.com>
2014-10-30 09:28:23 -0500

    update

--- rsBasePrep.ps1
+++ rsBasePrep.ps1
@@ -635,12 +635,14 @@ switch ($stage) {
    
    2
    {
+      Import-Module rsCommon
       Load-Globals
       Set-Stage -value 3
       Update-XenTools
    }
    3
    {
+      Import-Module rsCommon
       Load-Globals
       Disable-MSN
       Disable-TOE
@@ -657,6 +659,7 @@ switch ($stage) {
    }
    4
    {
+      Import-Module rsCommon
       Load-Globals
       Clean-Up
       Break
