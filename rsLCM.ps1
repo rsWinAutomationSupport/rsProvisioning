3edec1afcea6da7f928a627ba2b2655a442440cd
Alexei Andreyev <alexei.andreyev@gmail.com>
2015-01-05 15:38:09 +0000

    Client cert push retry logic added to improve client certificate exchange process reliability. Related to issue #30

--- rsLCM.ps1
+++ rsLCM.ps1
@@ -43,6 +43,8 @@ Configuration PullServerLCM
 . "$("C:\DevOps", $d.mR, 'PullServerInfo.ps1' -join '\' )"
 New-rsEventLogSource -logSource LCM
 
+$gitPath = "C:\Program Files (x86)\Git\bin\git.exe"
+
 if(Test-rsCloud) {
    $ObjectGuid = (Get-rsXenInfo -value name) -replace "instance-", ""
 }
@@ -63,19 +65,56 @@ else {
    $Node = $env:COMPUTERNAME
    $cN = "CN=" + $Node + "_enc"
    Set-Location -Path ("C:\DevOps", $d.mR -join "\")
-   Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "fetch origin $($d.branch_rsConfigs)"
-   Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "merge remotes/origin/$($d.branch_rsConfigs)"
+   Start -Wait $gitPath -ArgumentList "fetch origin $($d.branch_rsConfigs)"
+   Start -Wait $gitPath -ArgumentList "merge remotes/origin/$($d.branch_rsConfigs)"
    
    if (!(Test-Path -Path $("C:\DevOps", $d.mR, "Certificates", "Credentials" -join '\')))
    {
       New-Item -Path $("C:\DevOps", $d.mR, "Certificates", "Credentials" -join '\') -ItemType directory
    }
    powershell.exe "C:\DevOps\rsProvisioning\makecert.exe" -r -pe -n $cN -sky exchange -ss my $("C:\DevOps", $d.mR, "Certificates\Credentials","$ObjectGuid.cer"  -join '\'), -sr localmachine, -len 2048
-   Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "add $("C:\DevOps", $d.mR, "Certificates\Credentials","$ObjectGuid.cer"  -join '\')"
-   Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "commit -a -m `"pushing $ObjectGuid.crt`""
+   Start -Wait $gitPath -ArgumentList "add $("C:\DevOps", $d.mR, "Certificates\Credentials","$ObjectGuid.cer"  -join '\')"
+   Start -Wait $gitPath -ArgumentList "commit -a -m `"pushing $ObjectGuid.crt`""
+   
+   <# Commenting out below commands as they have been replaced by new logic below
    Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "fetch origin $($d.branch_rsConfigs)"
    Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "merge remotes/origin/$($d.branch_rsConfigs)"
    Start -Wait "C:\Program Files (x86)\Git\bin\git.exe" -ArgumentList "push origin $($d.branch_rsConfigs)"
+   #>
+   
+   # Add a random wait between cert sync attempts, if any of the git commands return a non-zero exit code
+   #
+    $certSyncRetries = 5
+    $certSyncAttempt = 0
+    
+    Do
+    {
+        $gitFetch = Start-Process -WindowStyle Hidden -PassThru -Wait $gitPath -ArgumentList commit "fetch origin $($d.branch_rsConfigs)"
+        $gitMerge = Start-Process -WindowStyle Hidden -PassThru -Wait $gitPath -ArgumentList commit "merge remotes/origin/$($d.branch_rsConfigs)"    
+        $gitPush = Start-Process -WindowStyle Hidden -PassThru -Wait $gitPath -ArgumentList commit "push origin $($d.branch_rsConfigs)"
+
+        if (($gitFetch.ExitCode -eq 0) -or ($gitMerge.ExitCode -eq 0) -or ($gitPush.ExitCode -eq 0))
+        {
+            $gitSyncSuccess = $true
+        }
+        else
+        {
+            $gitSyncSuccess = $false
+            Write-EventLog -LogName DevOps -Source LCM -EntryType Error -EventId 1000` -Message "Client certificate git push attempt failed... `n Git FETCH: $($gitFetch.ExitCode) `n Git MERGE: $($gitMerge.ExitCode) `n Git PUSH: $($gitPush.ExitCode) `n Attempt: $certSyncAttempt"
+            Start-Sleep (Get-Random -Minimum 15 -Maximum 120)
+        }
+        $certSyncAttempt += 1
+    } Until (($certSyncAttempt -eq $certSyncRetries) -or ($gitSyncSuccess))
+
+    if (($gitSyncSuccess))
+    {
+        Write-EventLog -LogName DevOps -Source LCM -EntryType Information -EventId 1000` -Message "Client certificate push complete after $certSyncRetries attempt(s)."
+    }
+    else 
+    {
+        Write-EventLog -LogName DevOps -Source LCM -EntryType Error -EventId 1000` -Message "Client certificate push failed after $certSyncRetries attempts. `n Please Re-run rsLCM process manually to correct this as client MOF will not be generated for this host."
+    }
+   
    chdir "C:\Windows\Temp"
    $pullServerName = $pullServerInfo.pullServerName
    $pullServerUri = "https://" + $pullServerName + ":8080/PSDSCPullServer.svc"
